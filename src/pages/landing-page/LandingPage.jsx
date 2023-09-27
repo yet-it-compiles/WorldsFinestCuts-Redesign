56f4f903c9ca78828c872892758f99ddcb22baa5
yet-it-compiles <tyler.unsworth@cwu.edu>
2023-09-26 21:23:05 -0700

    🚀Feat: Create Landing Page
    Create landing page, along with adding initial styling

---
+++ src/pages/landing-page/LandingPage.jsx
@@ -0,0 +1,37 @@
+/**
+ * @file LandingPage.jsx
+ *  
+ * @description This file is responsible for 
+ *  
+ * @requires react
+ * @requires LandingPage.module.scss
+ *  
+ * @exports LandingPage
+ */
+
+import React, { useState } from 'react';
+
+import styles from "./landing.module.scss"
+
+/**
+ * @component LandingPage
+ *
+ * @description Responsible for 
+ *
+ * Accomplished by 
+ *
+ * @returns {JSX.Element} - An element that resembles 
+ */
+const LandingPage = () => {
+  return (
+    <main>
+        <section>
+        <h1>Elevate Your Culinary Experience <br/> With World's Finest Cuts</h1>
+        <h2>Discover a world of flavor with our premium, sustainably sourced <br/>meats and seafood, delivered fresh from our farms to your table.</h2>
+
+        <button type="submit">Explore Our Selection</button>
+        </section>
+    </main>
+  );
+};
+export default LandingPage;
