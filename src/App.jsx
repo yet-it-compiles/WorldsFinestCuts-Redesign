aa4ee553a12ff031ca108444d51587688bc46df4
yet-it-compiles <tyler.unsworth@cwu.edu>
2023-11-07 07:59:21 -0800

    🚀Feat: Commit New App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,37 @@
 /**
  * @module App
- * 
- * @description Serves as the root container for the application and is 
+ *
+ * @description Serves as the root container for the application and is
  * responsible for rendering the overall layout and structure.
- * 
+ *
  * @version 0.1.0
- * 
+ *
  * @exports App
  */
 import LandingPage from "./pages/landing-page/LandingPage";
 import AboutUs from "./pages/about-us/AboutUs";
 import Product from "./components/product-display/Product";
+import SpecialOffers from "./pages/special-offers/SpecialOffers";
+import Checkout from "./pages/checkout/Checkout";
 
 /**
  * @function App
- * 
- * @description A functional component that renders the main structure of the 
+ *
+ * @description A functional component that renders the main structure of the
  * applications UI.
- *  
+ *
  * @returns {JSX.Element} The applications UI components
  */
 const App = () => {
-  return (
-    <>
-      <LandingPage/>
-      <AboutUs/>
-      <Product/>
-    </>
-  );
+    return (
+        <>
+            <LandingPage />
+            {/* <AboutUs/> */}
+            {/* <SpecialOffers/> */}
+            {/* <Product /> */}
+            {/* <Checkout /> */}
+        </>
+    );
 };
 
 export default App;
--- src/pages/landing-page/LandingPage.jsx
+++ src/pages/landing-page/LandingPage.jsx
@@ -1,22 +1,22 @@
 /**
  * @file LandingPage.jsx
- *  
- * @description This file is responsible for structuring the layout of the 
+ *
+ * @description This file is responsible for structuring the layout of the
  * landing page
- *  
+ *
  * @requires react
  * @requires landing.module.scss
- *  
+ *
  * @exports LandingPage
  */
 
-import React from 'react';
+import React from "react";
 
 import styles from "./landing.module.scss";
 import logo from "../../assets/WFC-Logo.svg";
 import Header from "../../components/header/Header";
 
-import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
+import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
 
 /**
  * @component LandingPage
@@ -26,38 +26,48 @@ import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
  * @returns {JSX.Element} The landing page
  */
 const LandingPage = () => {
-  return (
-    <>
-      <Header/>
-      <main className={styles.landing}>
-      <img src={logo} alt="World's Finest Cuts Logo" />
-      
-       <section className={styles.socials}>
-        <div className={styles.decoration}></div>
-          <section className={styles.icons}>
-            <a href='https://www.facebook.com/michael.dunn.1420'><FaFacebookF/></a>
-            <a href='https://www.facebook.com/michael.dunn.1420'><FaTwitter/></a>
-            <a href='https://www.facebook.com/michael.dunn.1420'><FaInstagram/></a>
-            
-            
-          </section>
-          <div className={styles.decoration}></div>
-       </section>
-      
-      <section className={styles.content}>
-        <h2>From The Ranch To Your Table</h2>
-        <h1><span>Elevate Your Culinary Experience</span> <br/> with World's Finest Cuts</h1>
-        <p>Discover a world of flavor with our premium, sustainably sourced <br/>meats and seafood, delivered fresh from our farms to your table.</p>
+    return (
+        <>
+            <Header />
+            <main className={styles.landing}>
+                <img src={logo} alt="World's Finest Cuts Logo" />
 
-        <div>
-          <button type="button">Explore Our Selection</button>
-        </div>
-      </section>
+                <section className={styles.socials}>
+                    <div className={styles.decoration}></div>
+                    <section className={styles.icons}>
+                        <a href="https://www.facebook.com/michael.dunn.1420">
+                            <FaFacebookF />
+                        </a>
+                        <a href="https://www.facebook.com/michael.dunn.1420">
+                            <FaTwitter />
+                        </a>
+                        <a href="https://www.facebook.com/michael.dunn.1420">
+                            <FaInstagram />
+                        </a>
+                    </section>
+                    <div className={styles.decoration}></div>
+                </section>
 
-      </main>
-    </>
-    
-  );
+                <section className={styles.content}>
+                    <h2>From The Ranch To Your Table</h2>
+                    <h1>
+                        <span>Elevate Your Culinary Experience</span> <br />{" "}
+                        with World's Finest Cuts
+                    </h1>
+                    <p>
+                        Discover a world of flavor with our premium, sustainably
+                        sourced <br />
+                        meats and seafood, delivered fresh from our farms to
+                        your table.
+                    </p>
+
+                    <div>
+                        <button type="button">Explore Our Selection</button>
+                    </div>
+                </section>
+            </main>
+        </>
+    );
 };
 
 export default LandingPage;
