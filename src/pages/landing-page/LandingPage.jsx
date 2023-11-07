34aa2058aedd3d4a8b668d91c67354ad065f8c64
yet-it-compiles <tyler.unsworth@cwu.edu>
2023-11-07 08:19:22 -0800

    🚀Feat: SEO Landing Page

--- src/pages/landing-page/LandingPage.jsx
+++ src/pages/landing-page/LandingPage.jsx
@@ -11,7 +11,7 @@
  */
 
 import React from "react";
-
+import { Helmet } from "react-helmet";
 import styles from "./landing.module.scss";
 import logo from "../../assets/WFC-Logo.svg";
 import Header from "../../components/header/Header";
@@ -28,6 +28,32 @@ import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
 const LandingPage = () => {
     return (
         <>
+            <Helmet>
+                <title>World's Finest Cuts - Premium Meat and Seafood</title>
+                <meta
+                    name="description"
+                    content="Discover a world of flavor with World's Finest Cuts. Our premium, sustainably sourced meats and seafood are delivered fresh from our farms to your table."
+                />
+                <link rel="canonical" href="https://www.worldsfinestcuts.com" />
+                <meta
+                    property="og:title"
+                    content="World's Finest Cuts - Premium Meat and Seafood"
+                />
+                <meta
+                    property="og:description"
+                    content="Explore our premium selection of meats and seafood. Sustainably sourced and delivered fresh."
+                />
+                <meta
+                    property="og:image"
+                    content="https://www.worldsfinestcuts.com/path-to-image.jpg"
+                />
+                <meta
+                    property="og:url"
+                    content="https://www.worldsfinestcuts.com"
+                />
+                <meta property="og:type" content="website" />
+                <meta name="twitter:card" content="summary_large_image" />
+            </Helmet>
             <Header />
             <main className={styles.landing}>
                 <img src={logo} alt="World's Finest Cuts Logo" />
@@ -35,13 +61,25 @@ const LandingPage = () => {
                 <section className={styles.socials}>
                     <div className={styles.decoration}></div>
                     <section className={styles.icons}>
-                        <a href="https://www.facebook.com/michael.dunn.1420">
+                        <a
+                            href="https://www.facebook.com/worldsfinestcuts"
+                            rel="nofollow noopener noreferrer"
+                            target="_blank"
+                        >
                             <FaFacebookF />
                         </a>
-                        <a href="https://www.facebook.com/michael.dunn.1420">
+                        <a
+                            href="https://twitter.com/worldsfinestcuts"
+                            rel="nofollow noopener noreferrer"
+                            target="_blank"
+                        >
                             <FaTwitter />
                         </a>
-                        <a href="https://www.facebook.com/michael.dunn.1420">
+                        <a
+                            href="https://instagram.com/worldsfinestcuts"
+                            rel="nofollow noopener noreferrer"
+                            target="_blank"
+                        >
                             <FaInstagram />
                         </a>
                     </section>
@@ -62,7 +100,13 @@ const LandingPage = () => {
                     </p>
 
                     <div>
-                        <button type="button">Explore Our Selection</button>
+                        <button
+                            type="button"
+                            className={styles.ctaButton}
+                            aria-label="Explore our selection of premium meats and seafood"
+                        >
+                            Explore Our Selection
+                        </button>
                     </div>
                 </section>
             </main>
